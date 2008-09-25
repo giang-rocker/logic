2995f55df6f9f3c562f5942906b49bc441fc3496
phuocsim@gmail.com <phuocsim@gmail.com@dbb475bd-3156-0410-82f6-7fd05290ab95>
2008-09-25 18:39:16 +0000

    coi lai 3 files nay nhe then cui bap..

--- trunk/Analyser/Scanner.cpp
+++ trunk/Analyser/Scanner.cpp
@@ -3,6 +3,8 @@
 //////////////////////////////////////////////////////////////////////
 
 #include "Scanner.h"
+#include <stdio.h>
+
 
 //////////////////////////////////////////////////////////////////////
 // Construction/Destruction
@@ -10,7 +12,7 @@
 
 Scanner::Scanner()
 {
-
+	
 }
 
 Scanner::~Scanner()
@@ -23,8 +25,193 @@ Scanner::Scanner(const string &text)
 	
 }
 
-int Scanner::NextToken(Token& t)
+char Scanner::nextChar()
+{	
+	position.m_iCharFinish++;
+	return buff.goAheadOneChar();
+}
+
+char Scanner::goAheadOneChar()
 {
-	
-	return 0;
+	char c = buff.goAheadOneChar();		
+	buff.bcurrent--;
+	return c;
+}
+
+void Scanner::goBackOneChar()
+{
+	buff.bcurrent--;
+}
+
+bool Scanner::isLetter(char c)
+{
+	return ('a'<=c && c <='z')||('A'<=c&& c<='Z');
+}
+bool Scanner::isLetterOrDigit(char c)
+{
+	int code = (int) c;
+	return ((int)'a' <= code && code <= (int)'z' ) || ((int)'A' <= code && code <= (int)'Z' ) || ((int)'0' <= code && code <= (int)'9' ) ;
+}
+
+Token Scanner::nextToken()
+{
+	Token result;
+	char c= NULL;
+	char t= NULL;
+	while (true)
+	{
+		switch (m_state)
+		{
+		case 0 :
+			c = nextChar();
+			m_text=c;
+                if (c == ' ' || c == '\t' || c== '\r' || c == '\n' ) {
+                    if (c == '\n') {
+                    	    position.NewLine();
+                    	}	          	    
+                    else if(c== '\t'){
+                    	position.m_iCharStart += 8;
+                    	position.m_iCharFinish = position.m_iCharStart-1;
+						}
+	                else {
+	                	position.m_iCharStart++;//charFinish has already increased by 1
+						}
+					}				
+				
+				else if (isLetter(c)) {
+						m_state = 1;
+					}
+				else if (c== '=') {
+						c = goAheadOneChar();
+						if(c== '>'){
+								m_text += c;
+								c=nextChar();
+								result = Token(MAPPING_OP,m_text,position);
+								position.m_iCharFinish++;
+								position.EndToken();
+								return result;
+							}
+						else{
+								result = Token(ERROR,m_text,position)	;
+								position.m_iCharFinish++;
+								position.EndToken();
+								return result;
+							}
+					}
+				else if (c== '<') {
+						c = goAheadOneChar();
+						if(c== '>'){ // <>
+								m_text += c;
+								c=nextChar();
+								result = Token(CONTRADITION_OP,m_text,position);
+								position.m_iCharFinish++;
+								position.EndToken();
+								return result;
+							}
+						else if (c == '='){ //<=
+								m_text += c;
+								c = goAheadOneChar();
+								if (c == '>') { // <=>
+										m_text += c;
+										c = nextChar();
+										c = nextChar();
+										result = Token(EQUIVALENT_OP,m_text,position);
+										position.EndToken();
+										return result;
+									}
+								else {
+										result = Token(ERROR,m_text,position)	;
+										position.m_iCharFinish++;
+										position.EndToken();
+										return result;
+									}
+							}
+								
+						else{
+								result = Token(ERROR,m_text,position)	;
+								position.m_iCharFinish++;
+								position.EndToken();
+								return result;
+							}
+					}
+				else if( c=='&'){
+						result = Token(INTERSECTION_OP,"&",position);
+						position.m_iCharFinish++;
+						position.EndToken();
+						return result;
+					}
+				else if( c=='|'){
+						result = Token(UNION_OP,"|",position);
+						position.m_iCharFinish++;
+						position.EndToken();
+						return result;
+					}
+				else if( c=='!'){
+						result = Token(NEGATION_OP,"!",position);
+						position.m_iCharFinish++;
+						position.EndToken();
+						return result;
+					}
+				else if( c=='('){
+						result = Token(LEFTPAR,"(",position);
+						position.m_iCharFinish++;
+						position.EndToken();
+						return result;
+					}
+				else if( c==')'){
+						result = Token(RIGHTPAR,")",position);
+						position.m_iCharFinish++;
+						position.EndToken();
+						return result;
+					}
+				else{
+						result = Token(ERROR,m_text,position)	;
+						position.m_iCharFinish++;
+						position.EndToken();
+						return result;
+					}
+		case 1:
+				m_text = "";
+				while (true){
+						if (!(isLetterOrDigit(c) || c == '_'))
+							break;
+						else{
+	                			m_text += c;
+							}
+						c = nextChar();
+					}
+				if (m_text == "AND" || m_text == "and")
+						result = Token(INTERSECTION_OP,m_text,SourcePosition(position.m_iCharStart,position.m_iCharFinish-1,position.m_iLineStart,position.m_iLineFinish));
+				else if (m_text == "OR" || m_text == "or")
+						result = Token(UNION_OP,m_text,SourcePosition(position.m_iCharStart,position.m_iCharFinish-1,position.m_iLineStart,position.m_iLineFinish));
+				else if (m_text == "NOT" )
+						result = Token(NEGATION_OP,m_text,SourcePosition(position.m_iCharStart,position.m_iCharFinish-1,position.m_iLineStart,position.m_iLineFinish));
+				else if (m_text == "true" || m_text == "TRUE"|| m_text == "FALSE"|| m_text == "false")
+						result = Token(BOOLEANLITERAL,m_text,SourcePosition(position.m_iCharStart,position.m_iCharFinish-1,position.m_iLineStart,position.m_iLineFinish));
+				else 
+						result = Token(IDENTIFER,m_text,SourcePosition(position.m_iCharStart,position.m_iCharFinish-1,position.m_iLineStart,position.m_iLineFinish));
+
+				position.EndToken();
+				goBackOneChar();
+				return result;
+		}
+	}
+}
+
+
+Buffer::Buffer(string filename)
+{
+	FILE* file = fopen("filename","r");
+	fread(buffer,sizeof(char),1024*1024,file);
+	fclose(file);
+}
+Buffer::Buffer() 
+{
+	cout<<"Nhap bai toan: ";
+	cin>>buffer;
+}
+char Buffer::goAheadOneChar()
+{	
+	bcurrent++;
+	return buffer[bcurrent];
 }
--- trunk/Analyser/Token.cpp
+++ trunk/Analyser/Token.cpp
@@ -10,7 +10,7 @@
 
 Token::Token()
 {
-
+	m_lexeme="";
 }
 
 Token::~Token()
@@ -20,27 +20,35 @@ Token::~Token()
 
 Token::Token(TokenKind kind)
 {
-
+	m_kind = kind;
+	m_lexeme = "";
 }
 
 Token::Token(TokenKind kind, const string& lexeme)
 {
-
+	m_kind = kind;
+	m_lexeme = lexeme;
 }
 
 Token::Token(TokenKind kind, const string& lexeme, SourcePosition position)
 {
-
+	m_kind = kind;
+	m_lexeme = lexeme;
+	m_position = position;
 }
 
 Token::Token(const Token& t)
 {
-
+	m_kind = t.m_kind;
+	m_lexeme = t.m_lexeme;
+	m_position = t.m_position;
 }
 
 Token::operator =(const Token& t)
 {
-
+	m_kind = t.m_kind;
+	m_lexeme = t.m_lexeme;
+	m_position = t.m_position;
 }
 
 Token::operator string() const
