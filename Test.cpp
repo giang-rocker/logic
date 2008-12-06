471f6aedcd53013996e4daaa5e401d26e63138f7
zrcoder <zrcoder@dbb475bd-3156-0410-82f6-7fd05290ab95>
2008-12-06 03:56:46 +0000

    Backup before deducts OR

--- Test.cpp
+++ Test.cpp
@@ -10,7 +10,7 @@ int main()
 
 	// 	"P, Q, (P & Q) -> (R & S) |- S";
 	// 	"HCl & NaOH -> NaCl & H2O , C &O2 -> CO2,CO2 & H2O -> H2CO3, HCl, NaOH,O2,C |- H2CO3";
-	// 	"A , B|- F -> A&B ";
+	//	"A , B|- F -> A&B ";
 	// 	"F |- G -> F";
 	// 	"|- F -> F";
 	// 	"|- ((P->Q)->P)->P";
@@ -38,8 +38,10 @@ int main()
 	//	"F->(G->H)|- (F&G)->H";
 	//	"F->G |-(F&H)->(G&H)";
 	//	"( F | G ) | H |- F | ( G | H )";  
-		"F & ( G | H ) |- (F & G ) | ( F & H )";
+	//	"F & ( G | H ) |- (F & G ) | ( F & H )";
 	//	"F -> (G->H) , F, !H |- !G";
+	//"F | H, !F |- H";
+	"((A&B)&C)&D , B, C->A, B -> A |- A";
 	Scanner* scanner = new Scanner(text);
 	Parser* p = new Parser(scanner);
 	p->parse();
