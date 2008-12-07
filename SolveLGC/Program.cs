acddbeb64db96403729059741b047108cdca1b0c
phuocsim@gmail.com <phuocsim@gmail.com@dbb475bd-3156-0410-82f6-7fd05290ab95>
2008-12-07 19:17:30 +0000

    bao cao 8/12/08

--- SolveLGC/Program.cs
+++ SolveLGC/Program.cs
@@ -14,7 +14,7 @@ namespace Logic
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new SolveLGC());
         }
     }
 }
