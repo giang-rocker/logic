f281157f7d9cd19affa7691c268cad477597ca38
phuocsim@gmail.com <phuocsim@gmail.com@dbb475bd-3156-0410-82f6-7fd05290ab95>
2008-12-11 19:53:06 +0000

    -add form

---
+++ SolveLGC/Form2.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Logic
+{
+    public partial class frmInstruction : Form
+    {
+        public frmInstruction()
+        {
+            InitializeComponent();
+        }
+    }
+}
