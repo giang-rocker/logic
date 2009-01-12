5f5f9c51a5e292d6a3f8fa78b97a27f15f991fe0
phuocsim <phuocsim@dbb475bd-3156-0410-82f6-7fd05290ab95>
2009-01-12 03:31:02 +0000

    -Last version for Rudopl . - Start add space

--- Test.cpp
+++ Test.cpp
@@ -9,23 +9,29 @@ using namespace std;
 int main()
 {
 	string dummy = "";
-	string text = dummy+
-		"all x (kid(x) -> like(x,Santa)),"+	
-		"all x all y (like(x, Santa) &  reindeer (y) -> like (x, y)),"+
-		"reindeer(Rudolph) & rednose(Rudolph),"+
-		"all x (rednose(x) -> weird (x) | clown(x))," +
-		"all x (reindeer(x) -> ! clown(x)),"+
-		"all x( weird (x) -> !like (Nam,x))" +
-		"|-  !kid(Nam) ";
+	string text = dummy +
+	
+// 		"all x (kid(x) -> like(x,Santa)),"+	
+// 		"all x all y (like(x, Santa) &  reindeer (y) -> like (x, y)),"+
+// 		"reindeer(Rudolph) & rednose(Rudolph)," +
+// 		"all x (rednose(x) -> weird (x) | clown(x))," +
+// 		"all x (reindeer(x) -> ! clown(x)),"+
+// 		"all x( weird (x) -> !like (Nam,x))" +
+// 		"|-  !kid(Nam) ";
 
-//		"\\- x (p(x) | q(x) ) |- -] x p(x) | \\- x q(x)";
+//	"all x all y ( f(x)| g(x) ) |- all x exists y (f(x) | g(x)) "	
+//	"f(a) |- exists x f(x)";
+//	"f(x) |- exists x f(x)";
+//	"f(x)|g(x) |- exists x (f(x)|g(x))";
+
+// 		"\\- x (p(x) | q(x) ) |- -] x p(x) | \\- x q(x)";
 // 				"all x  all y all z ((le(x, y) &le(y, z)) -> (!gt(x, z))),\n" +	dummy	+
 // 				"all x all y (le(x, y) -> (gt(y, x) | eq(x, y))),\n"		+	dummy	+
 // 				"all x all y ((gt(y, x) | eq(x, y)) ->le(x, y)),\n"			+	dummy	+
 // 				"all x exists y le(x, y) |- all x le(x,x)";
 //	"all x f(x) |- all y f(y)";
 //	"f(A) , g(B) , exists x( h(x) & f(x) & g(x)) |- exists y (f(y) & g(y))";
-//	" all y all x f(x,y) |- f(A,B)";
+	" all y all x f(x,y) |- f(A,B)";
 //	"! -] x (b(x) & g(x)), all x ( d(x) -> b(x)) |- ! -]x (d(x) & g(x))";
 //	"all x all y all z ((le(x, y) & le(y, z)) ->!gt(x, z)),all x all y (le(x, y) -> (gt(y, x) | eq(x, y))),all x all y ( (gt(y, x) | eq(x, y))->le(x, y)), all x exists y le(x, y), all x all y ( eq(x,y) -> eq(y,x))|- all x le(x, x)";
 //	"all x ( an (x) & ngu(x) -> heo(x)) , all x (nguoi(x) -> an (x) & lam (x) & ngu(x)) , nguoi (A) |- heo(A)";
@@ -53,17 +59,21 @@ int main()
 // 	"all x (p(x) -> q(x)), all x p(x) |- all x q(x)";
 // 	"p(t), all x (p(x) -> !q(x)) |- !q(t)";
 // 	"all x( p(x) & q(x)) |- all x p(x) & all x q(x) ";
-	" f(a), -] x (f(x)& g(x) & h(x)) |- -]x (g(x) & f(x))";
+//	"all x (p(x)|q(x)) |- all x p(x) | exists x q(x)";
+//	" f(a), -] x (f(x)& g(x) & h(x)) |- -]x (g(x) & f(x))";
+
+//		"F|G, R |- R|H";
+//		"F|G |- G|F";
 //	"p(A,B) -> h(A,B) |- p(A,B)|K -> h(A,B)|K";
 //	"A,B , A&B -> F |- F ->A";
-//	"A&B&C|- A";
+//	" A&B&C |- A ";
 //	"A1&A2->A3,A3|A1,A2|A3 |- A3 ";
 //	"F|F |- F";
 //	"A,B |- A & B";
 //	"A,B,A&B->C |- C";
 //	"(F|G)|H |- F|(G|H)";
 //	"P, Q, (P & Q) -> (R & S) |- S";
-//	"HCl & NaOH -> NaCl & H2O , C &O2 -> CO2,CO2 & H2O -> H2CO3, HC, NaOH,O2,C |- H2CO3";
+	"HCl & NaOH -> NaCl & H2O , C &O2 -> CO2,CO2 & H2O -> H2CO3, HCl, NaOH,O2,C |- H2CO3";
 //	"A , B |- (F -> (A&B)) & (G -> (A&B)&(A&B)) ";
 //	"F |- G -> F";
 //	"|- F -> F";
@@ -85,35 +95,37 @@ int main()
 // 	"|- F | !F";
 // 	"!F | !G |- !(F & G)";
 // 	"F -> !F |- !F";
-// "(F & !G) -> H, !H,F |- G ";
-// 	"A,A->C,C->D,D->B |- B | A";
+ "(F & !G) -> H, !H,F |- G ";
+ "A,A->C,C->D,D->B |- B | A";
 // 	"F |- !!F";
 //	"!G->!F |- F ->!!G ";
 // 	"|- (G->H)->((!G->!F)->(F->H))";
 // 	"(F&G)->H |- F->(G->H)";
 //	"F->(G->H)|- (F&G)->H";
-//	"F->G |-(F&H)->(G&H)";
-//	"( F | G ) | H |- F | ( G | H )";  
-//	"F & ( G | H ) |- (F & G ) | ( F & H )";
-//	"F | H, !F |- H";
-//	"((A&B)&C)&D , B, C->A, B -> A |- A";
-//	"(P -> Q) -> (P & Q) |- (!P -> Q) & (Q -> P) ";
-//	"|- ((F->G) & F) -> G";
+	"F->G |-(F&H)->(G&H)";
+	"( F | G ) | H |- F | ( G | H )";  
+	"F & ( G | H ) |- (F & G ) | ( F & H )";
+	"F | H, !F |- H";
+	"((A&B)&C)&D , B, C->A, B -> A |- A";
+	"(P -> Q) -> (P & Q) |- (!P -> Q) & (Q -> P) ";
+	"|- ((F->G) & F) -> G";
 //	"P & Q & (!P | !Q) |- !P & !Q & ( P | Q )";
 //	"(P -> Q) & (P -> R) |- P -> (R & Q)";
 //	"P -> (R & Q) |-(P -> Q) & (P -> R) ";
 //	"P & (P -> (P & Q)) |- !P | !Q | (P & Q)";
 //	"!P | !Q | (P & Q) |- P & (P -> (P & Q)) ";
-//	"P -> (R & Q) |- (P -> Q) & (P -> R)";
-//	"!(P | (!P & Q)) |- !P & !Q";
-//	"!P & !Q|-!(P | (!P & Q))  ";
-//	"( P & Q) -> R |-  (!P | ! Q) | R";
-//	"  (!P | ! Q) | R |- ( P & Q) -> R ";
-//	" P & Q |- P->Q ";
-//	"A,B , A&B -> F  |- G | F ";
-//	"F -> G |- F|H -> G|H";
-//	" |- (P&Q)|(P&!Q)|(!P&Q)|(!P&!Q)";
-//	"P&Q->R |- P->(Q->R)";
+	"P -> (R & Q) |- (P -> Q) & (P -> R)";
+//	"!(P | (!P & Q)) |- !P & !Q";	
+	"!P & !Q|-!(P | (!P & Q))  ";
+
+	//Lems have trouble
+	"P,Q,(( P & Q) -> (!P | ! Q)) | R |-  (!P | ! Q) | R";
+	"  (!P | ! Q) | R |- ( P & Q) -> R ";
+	" P & Q |- P->Q ";
+	"A,B , A&B -> F ,G |- G|F ";
+	"F -> G |- F|H -> G|H";
+	" |- (P&Q)|(P&!Q)|(!P&Q)|(!P&!Q)";		//Have a trouble here
+	"P&Q->R |- P->(Q->R)";
 
 	"A |- B|A";
 	Scanner* scanner = new Scanner(text);
@@ -130,17 +142,20 @@ int main()
 		xWam bk = p->data;
 		if(nd.ProveIt())
 		{
-			cout<<text<<endl<<endl;
+			
 			cout<<nd.Result()<<endl<<endl<<endl;
+			cout<<text<<endl<<endl;
 		}
 		else 
 		{
+			return 0;
 			NaturalDeduction lem(bk);
 			lem.insertLEMs();
 			if(lem.ProveIt())
 			{
-				cout<<text<<endl<<endl;
+				
 				cout<<lem.Result()<<endl<<endl<<endl;
+				cout<<text<<endl<<endl;
 			}
 			else
 			{
